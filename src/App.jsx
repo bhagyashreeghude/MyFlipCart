c761b2ed395444affd78acf6f81a66506ec1afee
bhagyashree-ghude <bhagyashreeghude@gmail.com>
2024-06-06 18:05:50 +0530

    2.creating router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Cart from "./components/Cart";
+import Contact from "./components/Contact";
+import Products from "./components/Products";
+import SingleProduct from "./components/SingleProduct";
 function App() {
-  
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/products" element={<Products/>} />
+            <Route path="/singleproduct/:id" element={<SingleProduct/>} />
+            <Route path="/cart" element={<Cart/>} />
+            <Route path="/about" element={<About/>} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  );
 }
 
-export default App
+export default App;
