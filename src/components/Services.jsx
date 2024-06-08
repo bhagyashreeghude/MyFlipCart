cb2d5290ee8a3e527c50290ab8f18c74eb1159ea
bhagyashree-ghude <bhagyashreeghude@gmail.com>
2024-06-08 17:45:13 +0530

    feature services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,20 +5,20 @@ import { GiReceiveMoney } from "react-icons/gi";
 const Services = () => {
   return (
     <div className="  ">
-      <p>Our Feature Services</p>
+      <p className="text-3xl ml-40">Our Feature Services</p>
 
-      <div className="  ml-40 my-10  grid grid-cols-3 gap-2 w-[70%]">
-        <div className="col-span-1 bg-gray-200 p-2">
-          <TbTruckDelivery className="ml-16" size={40} />
-          <p>Super fast and Free Delivery</p>
+      <div className="ml-40 my-10  grid grid-cols-3 gap-2 w-[60%]">
+        <div className="col-span-1 bg-gray-200 p-2 rounded-full border border-black">
+          <TbTruckDelivery className="ml-28 " size={40} />
+          <p className="text-center">Super fast and Free Delivery</p>
         </div>
-        <div className="col-span-1 bg-gray-200">
-        <MdSecurity className="ml-16" size={40} />
-          <p>Non-Contact Shipping</p>
+        <div className="col-span-1 bg-gray-200 p-2 rounded-full border border-black">
+          <MdSecurity className="ml-32" size={35} />
+          <p className="text-center p-1">Non-Contact Shipping</p>
         </div>
-        <div className="col-span-1 bg-gray-200 rounded-full ">
-          <GiReceiveMoney className="ml-16" size={40} />
-          <p>Money-back Gauranteed</p>
+        <div className="col-span-1 bg-gray-200 p-2 rounded-full border border-black ">
+          <GiReceiveMoney className="ml-28" size={40} />
+          <p className="text-center">Money-back Gauranteed</p>
         </div>
       </div>
     </div>
